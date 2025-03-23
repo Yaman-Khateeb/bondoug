8b0ec338f04171fe3b1db771146c5c13c5d5a9a8
Yaman-Khateeb <143772940+Yaman-Khateeb@users.noreply.github.com>
2025-03-23 13:03:14 +0300

    Final version of project

--- DAL/DataAccessSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DAL
-{
-    public class DataAccessSettings
-    {
-        public static string ConnectionString = "Server=.;Database=BondouqeDB;User ID=sa;Password=sa123456;TrustServerCertificate=True;";
-        public static string ConnectionString2 = "Server=.;Database=BondouqeDB;Integrated Security=True;TrustServerCertificate=True;";
-
-    }
-}
